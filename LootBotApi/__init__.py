7feac2fedb760e97f52090cc55f913958f2f5698
Anatras02 <39670708+Anatras02@users.noreply.github.com>
2020-12-30 00:36:35 +0100

    Add files via upload

---
+++ LootBotApi/__init__.py
@@ -0,0 +1,7 @@
+__title__ = 'LootBotApi'
+__version__ = '0.1'
+__author__ = 'Anatras02'
+__license__ = 'MIT License'
+__copyright__ = 'Copyright 2020 Anatras02'
+
+from .LootBotApi import LootBotApi
